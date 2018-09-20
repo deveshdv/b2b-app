05d08efa6ff280deaa5612e4958395330c7433e6
Devesh Thakur <dthakur@op5.com>
2018-09-21 04:04:55 +0530

    added server folder and express app

---
+++ server/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+		'no-console': 'off',
+		'no-process-env': 'off'
+    }
+};
